f85f976d0e870db3519692f5d65d21534a40e15f
c4gi <111027053+c4gi@users.noreply.github.com>
2022-08-23 18:15:02 -0400

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -52,11 +52,26 @@
 		    }
 		});
 
-		window.addEventListener('Touch', function (evt) {
+		window.addEventListener('touchstart', function (evt) {
 		    if (evt.detail === 3) {
 			window.location.replace("http://google.com");
 		    }
 		});
+
+		document.getElementById("double-tap").addEventListener("touchstart", tapHandler);
+
+		var tapedTwice = false;
+
+		function tapHandler(event) {
+		    if(!tapedTwice) {
+			tapedTwice = true;
+			setTimeout( function() { tapedTwice = false; }, 300 );
+			return false;
+		    }
+		    event.preventDefault();
+		    //action on double tap goes below
+		    alert('You tapped me Twice !!!');
+		 }
 
 
    /* Mobile Menu
