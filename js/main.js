6699d8750bdb6f0b55db0c69ead5aed5f45a88de
c4gi <111027053+c4gi@users.noreply.github.com>
2022-08-23 21:39:48 -0400

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -51,18 +51,20 @@
 		    if (event.detail === 3) {
 			window.location.replace("http://google.com");
 		    }
-		});
+                });
 
 		window.addEventListener('touchstart', function (event) {
-                    event.preventDefault();
-		    if (event.detail === 2) {
-                        event.preventDefault();
-			return: false;
-		    }
 		    if (event.detail === 3) {
 			window.location.replace("http://google.com");
 		    }
 		});
+
+		$('.no-zoom').bind('touchend', function(e) {
+		  e.preventDefault();
+		  // Add your code here. 
+		  $(this).click();
+		  // This line still calls the standard click event, in case the user needs to interact with the element that is being clicked on, but still avoids zooming in cases of double clicking.
+		})
 
 
    /* Mobile Menu
